38bd04df568c79f6115077965bf222765614ed77
Kei Oikawa <hosikiti@gmail.com>
2023-03-07 13:19:27 +0900

    Remove svelte build folder from this repository.

--- src/frontend/build/client/_app/immutable/chunks/1.27b44eaf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.29acac45.js";export{t as component};
--- src/frontend/build/server/chunks/0-17d41816.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-const component = async () => (await import('./_layout.svelte-be8acfaa.js')).default;
-const file = '_app/immutable/entry/_layout.svelte.705e5bcc.js';
-const imports = ["_app/immutable/entry/_layout.svelte.705e5bcc.js","_app/immutable/chunks/index.c3faf347.js","_app/immutable/chunks/stores.1471ea71.js","_app/immutable/chunks/singletons.4df91814.js"];
-const stylesheets = ["_app/immutable/assets/_layout.1088a596.css"];
-const fonts = ["_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2","_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff","_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2","_app/immutable/assets/fira-mono-greek-ext-400-normal.9e2fe623.woff2","_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2","_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2","_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2"];
-
-export { component, file, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-17d41816.js.map
