880fe022d2dc5ac3f7432c6d645b8f934356d726
Kei Oikawa <hosikiti@gmail.com>
2023-03-19 10:55:16 +0900

    Adjust footer

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -10,11 +10,18 @@
 <div class="app">
 	<Header />
 
-	<main class="h-[calc(100vh-12rem)]">
+	<main>
 		<slot />
 	</main>
 
-	<footer class="fixed bottom-0 flex w-screen justify-center p-4">
-		<span class="text-slate-600 text-xs">(C) 2023 Kei Oikawa.</span>
+	<footer class="flex flex-col w-screen bg-slate-300 h-[8rem] p-2 pt-4 pb-8">
+		<span class="block font-bold font-serif text-xl">BriefCast</span>
+		<span class="block font-serif text-slate-500">Less news, more life.</span>
+		<span class="flex-1" />
+		<span class="text-slate-500 text-xs">© 2023 Kei Oikawa.</span>
 	</footer>
+
+	<!-- <footer class="flex w-screen justify-center p-2">
+		<span class="text-slate-600 text-xs">(C) 2023 Kei Oikawa.</span>
+	</footer> -->
 </div>
