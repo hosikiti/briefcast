f6a8188b83926206df47a6f8ab62e89279ded9b2
Kei Oikawa <hosikiti@gmail.com>
2023-03-31 13:14:30 +0900

    Fix hang issue while deployment

--- run_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "service restarting ..."
-docker-compose up -d --build
-echo "shutdown core server ..."
-docker-compose exec -T briefcast_core pkill deno
-echo "start server ..."
-docker-compose exec -T briefcast_core ./run_dev.sh &
-docker-compose exec -T briefcast npm run serve &
