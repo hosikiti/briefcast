0d98096beb20a80bd299c90b84da0b135281ff0f
Kei Oikawa <hosikiti@gmail.com>
2023-04-01 14:09:40 +0900

    Adjust styles

--- app/src/lib/components/AddPostModal.svelte
+++ app/src/lib/components/AddPostModal.svelte
@@ -26,34 +26,39 @@
 	}
 </script>
 
-<div
-	class="absolute z-[999] top-0 left-0 w-screen h-main bg-black bg-opacity-40 flex justify-center"
->
-	<div class="bg-white border p-4 shadow-lg m-8 lg:w-[60%] w-[80%] h-[60%] relative flex flex-col">
-		<h2>{formData.name || 'New podcast'}</h2>
-		<div class="flex flex-col my-4 gap-4">
-			<label class="label">
-				<span>Name: </span>
-				<input type="url" class="input p-2" placeholder="Feed title" bind:value={formData.name} />
-			</label>
-			<label class="label">
-				<span>Feed URL: </span>
-				<input
-					type="url"
-					class="input p-2"
-					placeholder="RSS/Atom feed URL"
-					bind:value={formData.feedUrl}
-				/>
-			</label>
-			<label class="label" for="">
-				<span>Podcast Language: </span>
-				<LangSelect bind:selectedLanguage />
-			</label>
-		</div>
-		<div class="flex-1" />
-		<div class="flex justify-end gap-2 items-end">
-			<button class="btn variant-filled bg-orange-500 text-white" on:click={onSubmit}>Add</button>
-			<button class="btn variant-soft" on:click={onCancel}>Cancel</button>
-		</div>
+<div class="bg-white border p-4 shadow-lg  lg:w-[60%] w-[80%] relative flex flex-col">
+	<h2>{formData.name || 'New podcast'}</h2>
+	<div class="flex flex-col my-4 gap-4">
+		<label class="label">
+			<span>Name: </span>
+			<input type="text" class="input p-2" placeholder="Feed title" bind:value={formData.name} />
+		</label>
+		<label class="label">
+			<span>Feed URL: </span>
+			<input
+				type="url"
+				class="input p-2"
+				placeholder="RSS/Atom feed URL"
+				bind:value={formData.feedUrl}
+			/>
+		</label>
+		<label class="label" for="">
+			<span>Podcast Language: </span>
+			<LangSelect bind:selectedLanguage />
+		</label>
+		<label class="label">
+			<span>Prompt for your podcast transcript: </span>
+			<textarea class="textarea" rows="5" />
+		</label>
+	</div>
+	<div class="flex-1" />
+	<div class="flex justify-end gap-2 items-end">
+		<button class="btn variant-filled bg-orange-500 text-white" on:click={onSubmit}>Add</button>
+		<button class="btn variant-soft" on:click={onCancel}>Cancel</button>
 	</div>
 </div>
+
+<!-- <div
+	class="absolute z-[999] top-0 left-0 w-screen h-main bg-black bg-opacity-40 flex justify-center"
+>
+</div> -->
