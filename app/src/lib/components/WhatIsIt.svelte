8e509348ad4e20861cc772e7b950e243e5a2a231
Kei Oikawa <hosikiti@gmail.com>
2023-04-14 23:22:39 +0900

    Adjust styles.

--- app/src/lib/components/WhatIsIt.svelte
+++ app/src/lib/components/WhatIsIt.svelte
@@ -26,26 +26,26 @@
 	</div>
 </div>
 <div class="bg-slate-700 w-full text-white flex flex-col md:items-center">
-	<div class="flex flex-col w-full md:w-[60%] items-start md:items-center px-8">
+	<div class="flex flex-col w-full md:w-[60%] items-center  px-8">
 		<span class="my-8 font-bold text-slate-50">Sign-up for free, and:</span>
 		<div class="text-left md:px-16">
 			<ul>
-				<li>You can create multiple podcasts from news and blog or whatever website you like.</li>
+				<li>You can create multiple podcasts from news, blog, and whatever.</li>
 				<li>Your podcasts are saved in a playlist.</li>
-				<li>Your podcasts are customizable and updated automatically.</li>
+				<li>Your podcasts are customizable and updated every day.</li>
 			</ul>
 		</div>
-	</div>
-	<div class="my-8 px-8 md:w-[240px] flex-col flex">
-		<button
-			class="btn variant-filled bg-orange-500 text-white rounded-xl"
-			on:click={() => goto('/signin')}
-		>
-			Get started</button
-		>
-		<span class="py-4 text-center text-slate-400 font-semibold text-sm"
-			>No credit card required</span
-		>
+		<div class="my-8 px-8 md:w-[240px] flex-col flex">
+			<button
+				class="btn variant-filled bg-orange-500 text-white rounded-xl"
+				on:click={() => goto('/signin')}
+			>
+				Get started</button
+			>
+			<span class="py-4 text-center text-slate-400 font-semibold text-sm"
+				>No credit card required</span
+			>
+		</div>
 	</div>
 </div>
 
